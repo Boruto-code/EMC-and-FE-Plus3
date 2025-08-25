2db0f7be826e33bcbd1171cd149f03332d5c35fc
Boruto-code <hamburger_nb_666@outlook.com>
2025-08-25 08:14:44 +0800

    Update 1.21

---
+++ kubejs/startup_scripts/items.js
@@ -0,0 +1,13 @@
+Platform.mods.kubejs.name = "EMC & FE +++"
+
+StartupEvents.registry("item", event => {
+    event.create("kubejs:copper_coin")
+    event.create("kubejs:iron_coin")
+    event.create("kubejs:gold_coin")
+    event.create("kubejs:diamond_coin")
+    event.create("kubejs:netherite_coin")
+
+    event.create("kubejs:strange_colored_ingot")
+
+    event.create("kubejs:incomplete_creative_blaze_cake")
+})
