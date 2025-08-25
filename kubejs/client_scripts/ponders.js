de9afe74b4186a6074ac9907584e1e94d18e7552
Boruto-code <hamburger_nb_666@outlook.com>
2025-08-25 08:14:44 +0800

    Update 1.21

---
+++ kubejs/client_scripts/ponders.js
@@ -0,0 +1,10 @@
+Ponder.registry(event => {
+    event.create("draconicevolution:crafting_core")
+        .scene(
+            "kubejs:crafting_core",
+            "聚合装置的搭建",
+            scene => {
+
+            }
+        )
+})
