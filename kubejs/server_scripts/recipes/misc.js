74e111e30c26f21298f02a369baa2e326a05fd76
Boruto-code <hamburger_nb_666@outlook.com>
2025-09-08 19:11:44 +0800

    Update

--- kubejs/server_scripts/recipes/misc.js
+++ kubejs/server_scripts/recipes/misc.js
@@ -3,4 +3,39 @@ ServerEvents.recipes(event => {
         Item.of("kubejs:awakened_scrap", 16),
         "draconicevolution:awakened_draconium_ingot"
     )
+
+    event.shapeless(
+        "kubejs:iron_coin",
+        [
+            "kubejs:copper_coin", "kubejs:copper_coin", "kubejs:copper_coin",
+            "kubejs:copper_coin", "kubejs:copper_coin", "kubejs:copper_coin",
+            "kubejs:copper_coin", "kubejs:copper_coin",  "kubejs:copper_coin"]
+    )
+
+    event.shapeless(
+        "kubejs:gold_coin",
+        [
+            "kubejs:iron_coin", "kubejs:iron_coin", "kubejs:iron_coin",
+            "kubejs:iron_coin", "kubejs:iron_coin", "kubejs:iron_coin",
+            "kubejs:iron_coin", "kubejs:iron_coin", "kubejs:iron_coin"
+        ]
+    )
+
+    event.shapeless(
+        "kubejs:diamond_coin",
+        [
+            "kubejs:gold_coin", "kubejs:gold_coin", "kubejs:gold_coin",
+            "kubejs:gold_coin", "kubejs:gold_coin", "kubejs:gold_coin",
+            "kubejs:gold_coin", "kubejs:gold_coin", "kubejs:gold_coin"
+        ]
+    )
+
+    event.shapeless(
+        "kubejs:netherite_coin",
+        [
+            "kubejs:diamond_coin", "kubejs:diamond_coin", "kubejs:diamond_coin",
+            "kubejs:diamond_coin", "kubejs:diamond_coin", "kubejs:diamond_coin",
+            "kubejs:diamond_coin", "kubejs:diamond_coin", "kubejs:diamond_coin"
+        ]
+    )
 })
